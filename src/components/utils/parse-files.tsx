46e780d71ba422938a4b4b917dbb687d97cbb6e3
Olutoye-lab <Olutoyeeresanmi@gmail.com>
2026-01-20 14:20:50 +0000

    Fixed excel file paring bug: data objects return raw text instad on normalised integers

--- src/components/utils/parse-files.tsx
+++ src/components/utils/parse-files.tsx
@@ -2,7 +2,7 @@ import Papa from "papaparse";
 import * as XLSX from 'xlsx'
 
 const parseCSV = async(file: File) => {
-    let data 
+    let data
     const text = await file.text()
     console.log(text)
     Papa.parse(text, {
@@ -18,29 +18,31 @@ const parseCSV = async(file: File) => {
         }}
     )
     return data
-
 }
 
 const parseExcel = async(file: File) => {
 
     const arrayBuffer = await file.arrayBuffer()
 
-    const workbook = XLSX.read(arrayBuffer, {type: "array"})
+    const workbook = XLSX.read(arrayBuffer, {type: "array", cellDates: true})
     const sheet = workbook.Sheets[workbook.SheetNames[0]]
 
-    const json: any[] = XLSX.utils.sheet_to_json(sheet, {header: 1})
-    
-    console.log("Json", json)
+    let json: any[] = XLSX.utils.sheet_to_json(sheet, {
+        header: 1, 
+        raw: false,
+        dateNF: 'yyyy-mm-dd"T"hh:mm:ss".000Z"'})
 
     const array = json.map((row)=>{
+
         const obj: any = {}
 
         for (let i = 0; i < json[0].length; i++){
             obj[json[0][i]] = row[i]
         }
+        console.log(obj)
         return obj
     })
-    return array
+    return array.slice(1)
 }
 
 export {parseCSV, parseExcel}
